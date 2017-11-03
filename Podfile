f7c59093571529dd56194ecec5033497910ae753
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-11-03 03:57:16 +0200

    add Facebook pods

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftFB' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'FacebookCore’, ‘~>0.2.0’
+  pod 'FacebookLogin’, ‘~>0.2.0’
+  pod 'FacebookShare', ‘~>0.2.0’
+  pod 'FBSDKCoreKit', '~> 4.22.1'
+  pod 'FBSDKLoginKit', '~> 4.22.1'
+  pod 'FBSDKShareKit', '~> 4.22.1'
+  
+  # Pods for SwiftFB
+
+end
--- Source/SwiftFB/AppDelegate.swift
+++ Source/SwiftFB/AppDelegate.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
+import FacebookCore
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -23,10 +24,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.backgroundColor = UIColor.purple
         
         window!.makeKeyAndVisible()
-    
+        
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
 
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return SDKApplicationDelegate.shared.application(application,
+                                                         open: url,
+                                                         sourceApplication: sourceApplication,
+                                                         annotation: annotation)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
