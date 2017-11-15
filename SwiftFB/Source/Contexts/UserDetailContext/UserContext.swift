0e81512466634cb7457d97e9e9d521ff03b439b3
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-11-15 22:09:40 +0200

    restructure project

---
+++ SwiftFB/Source/Contexts/UserDetailContext/UserContext.swift
@@ -0,0 +1,13 @@
+//
+//  UserContext.swift
+//  SwiftFB
+//
+//  Created by Aleksey Zuiev on 15/11/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+import UIKit
+
+class UserContext: GetContext {
+
+}
